62123e5aa5fc32392f52d350372cccfd48418e85
Joao Oliveira <joao.oliveira@unparallel.pt>
2020-01-17 15:56:59 +0000

    change firebase sdk path

--- src/main/java/OCARIoT/Main.java
+++ src/main/java/OCARIoT/Main.java
@@ -3,6 +3,7 @@ package OCARIoT;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,14 +18,15 @@ import java.security.KeyStoreException;
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class Main {
 
-
+    @Value("${firebase.adminsdk.path}")
+    private static String adminSDKPath;
 
     public static void main(String[] args) throws IOException, KeyStoreException {
 
 
         SpringApplication.run(Main.class,args);
 
-        FileInputStream serviceAccount = new FileInputStream("/Users/jpdoliveira/IdeaProjects/OCARIoT/src/main/resources/ocariot-3ecd2-firebase-adminsdk-nq31m-d53e9217cd.json");
+        FileInputStream serviceAccount = new FileInputStream(adminSDKPath);
         FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
         FirebaseApp.initializeApp(options);
 
