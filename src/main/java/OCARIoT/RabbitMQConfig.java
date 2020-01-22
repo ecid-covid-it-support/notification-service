9edf908086f759888fb4660ed3bab916204d3f71
Joao Oliveira <joao.oliveira@unparallel.pt>
2020-01-22 17:46:52 +0000

    no message

--- src/main/java/OCARIoT/RabbitMQConfig.java
+++ src/main/java/OCARIoT/RabbitMQConfig.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Configuration
 public class RabbitMQConfig {
 
-
+    private static final Logger LOGGER = Logger.getLogger( RabbitMQ.class.getName() );
 
     @Value("${rabbitmq.exchange.send.notification}")
     private String exchangeNotification;
@@ -33,7 +36,17 @@ public class RabbitMQConfig {
 
     @Bean
     public Binding binding1a(DirectExchange directNotification, Queue queueNotification) {
-        return BindingBuilder.bind(queueNotification).to(directNotification).with(routingkeyNotification);
+
+        Binding binder = null;
+        try{
+
+            binder = BindingBuilder.bind(queueNotification).to(directNotification).with(routingkeyNotification);
+            LOGGER.log(Level.WARNING, "Connection established.");
+
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Trying to establish connection.");
+        }
+        return binder;
     }
 
 }
