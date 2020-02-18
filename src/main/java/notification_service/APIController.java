7a34078e923c80fbbbd77843e425a402d7d7b623
Joao Oliveira <joao.oliveira@unparallel.pt>
2020-02-18 12:23:46 +0000

    Added support for Language and Timestamp

--- src/main/java/notification_service/APIController.java
+++ src/main/java/notification_service/APIController.java
@@ -1,12 +1,16 @@
 package notification_service;
 
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
 import static com.mongodb.client.model.Filters.eq;
 import com.mongodb.client.model.UpdateOptions;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 import java.util.Map;
 
 
@@ -23,11 +27,30 @@ public class APIController {
     }
 
     @PostMapping("/v1/notifications/user/{id}")
-    public String create(@PathVariable String id, @RequestBody Map <String,String> body) {
+    public ResponseEntity<String> create(@PathVariable String id, @RequestBody Map <String,String> body) {
 
-        String token = body.get("token");
-        collection.updateOne(eq("id", id), new Document("$addToSet", new Document("Tokens",token)),new UpdateOptions().upsert(true));
-        return "User saved";
+                String token = body.get("token");
+                String lang = body.get("lang");
+                if (token==null || token.isEmpty()){
+
+                   return ResponseEntity.status(400).body("No token found in the message body");
+                }
+                if (lang==null || lang.isEmpty()){
+
+                    return ResponseEntity.status(400).body("No lang found in the message body");
+                }
+                if (!lang.equals("en")&&!lang.equals("es")&&!lang.equals("pt")&&!lang.equals("el")){
+
+                    return ResponseEntity.status(400).body("Only English(en), Spanish(es), Portuguese(pt), Greek(el) languages supported");
+                }
+                collection.updateOne(eq("id", id), new Document("$addToSet", new Document("Tokens", token)), new UpdateOptions().upsert(true));
+                BasicDBObject updateFields = new BasicDBObject();
+                updateFields.append("lang", lang).append("ts", new Date());
+                BasicDBObject setQuery = new BasicDBObject();
+                setQuery.append("$set", updateFields);
+                collection.updateOne(eq("id", id), setQuery);
+                return ResponseEntity.status(200).body("User saved or updated");
+
     }
 
 }
