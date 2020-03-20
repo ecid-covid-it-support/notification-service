be28859193740f710bd383c7c5c464240b525001
Joao Oliveira <joao.oliveira@unparallel.pt>
2020-03-20 15:36:50 +0000

    Created a RabbitMQ requester to use RPC from other microservice

---
+++ src/main/java/notification_service/RabbitMQRequester.java
@@ -0,0 +1,49 @@
+package notification_service;
+
+import org.json.JSONObject;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+
+@Component
+public class RabbitMQRequester {
+
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+
+    public String send(String handle, String method) {
+
+        Message response = null;
+        JSONObject jsonObject= null;
+
+        try {
+
+
+            JSONObject myObj = new JSONObject();
+            myObj.put("resource_name", method);
+            myObj.append("handle", handle);
+
+            Message message = MessageBuilder.withBody(myObj.toString().getBytes())
+                    .setContentType("application/json")
+                    .build();
+
+            response = rabbitTemplate.sendAndReceive("account.rpc", method, message);
+
+
+
+        } catch (AmqpException e) {
+            e.printStackTrace();
+        }
+
+        return new String(response.getBody());
+
+
+    }
+}
