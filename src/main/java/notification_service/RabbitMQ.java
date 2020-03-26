87f547ff272223c60bc8c3379972076fc5528a2f
Joao Oliveira <joao.oliveira@unparallel.pt>
2020-03-26 18:00:10 +0000

    fix Children query and remove ApplicationContextUtils class

--- src/main/java/notification_service/RabbitMQ.java
+++ src/main/java/notification_service/RabbitMQ.java
@@ -110,7 +110,8 @@ public class RabbitMQ{
                                         String uID = jsonmsg.getString("id");
                                         int days_since = jsonmsg.getInt("days_since");
 
-                                        String info = rabbitMQRequester.send("_id="+uID,"children.find");
+                                        String info = rabbitMQRequester.send("?_id="+uID,"children.find");
+                                        
 
                                         JSONArray jsonarray = new JSONArray(info);
 
