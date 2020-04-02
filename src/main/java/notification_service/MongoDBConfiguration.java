56b4f7da479b02d62ac35d0a5391f84eb6bccc80
Joao Oliveira <joao.oliveira@unparallel.pt>
2020-04-02 13:45:02 +0100

    removed file import

--- src/main/java/notification_service/MongoDBConfiguration.java
+++ src/main/java/notification_service/MongoDBConfiguration.java
@@ -5,19 +5,10 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
@@ -48,7 +39,7 @@ public class MongoDBConfiguration{
         System.setProperty ("javax.net.ssl.keyStorePassword",keystorePass);
         System.setProperty ("javax.net.ssl.trustStore",truststorePath);
         System.setProperty ("javax.net.ssl.trustStorePassword","changeit");
-        MongoClient mongoClient = MongoClients.create(mongoURI+"&sslInvalidHostNameAllowed=true&authSource="+mongoDatabase);
+        MongoClient mongoClient = MongoClients.create(mongoURI+"&sslInvalidHostNameAllowed=true");
         return mongoClient.getDatabase(mongoDatabase);
     }
 
@@ -65,7 +56,7 @@ public class MongoDBConfiguration{
     @Bean
     public MongoCollection<Document> messagesCollection(MongoDatabase database) {
 
-        MongoCollection<Document> messages = database.getCollection("messages");
+        /*MongoCollection<Document> messages = database.getCollection("messages");
 
         try{
             List<JSONObject> documents = new ArrayList<>();
@@ -84,7 +75,7 @@ public class MongoDBConfiguration{
 
         } catch (IOException | ParseException e) {
             LOGGER.log(Level.WARNING, "Could get Messages file");
-        }
+        }*/
 
         return database.getCollection("messages");
 
